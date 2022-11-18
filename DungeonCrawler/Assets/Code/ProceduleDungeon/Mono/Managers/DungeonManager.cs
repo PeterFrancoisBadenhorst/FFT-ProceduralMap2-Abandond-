f67fe95731a5a00f82c5c40743a923cb6ed31d63
Peter Francois Badenhorst <peter.francois.badenhorst@gmail.com>
2022-11-18 19:05:30 +0200

    Create Grid

---
+++ DungeonCrawler/Assets/Code/ProceduleDungeon/Mono/Managers/DungeonManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Assets.Code.ProceduleDungeon.Utilities;
+using Unity.VisualScripting;
+
+namespace Assets.Code.ProceduleDungeon.Mono.Managers
+{
+    internal class DungeonManager : MonoBehaviour
+    {
+        public GameObject TempPrefab;
+        public Transform GridParent;
+        public int GridSize;
+        public float GridScale;
+
+
+        private void Start()
+        {
+           /// if (GridParent == null) this.transform = GridParent;
+           var grid = GridCreate.SquareGrid2DHorizontal(GridSize, GridScale);
+            for (int i = 0; i < grid.Length; i++)
+            {
+              var t =  Instantiate(TempPrefab, grid[i],Quaternion.identity);
+                t.transform.SetParent(GridParent);
+            }
+        }
+
+
+    }
+}
---
+++ DungeonCrawler/Assets/Code/ProceduleDungeon/Utilities/GridCreate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code.ProceduleDungeon.Utilities
+{
+    internal class GridCreate
+    {
+        public static Vector3[] SquareGrid2DVertical(int gridSize, float scale)
+        {
+            var createdTransforms = new List<Vector3>();
+            for (int X = 0; X < gridSize; X++)
+            {
+                for (int Y = 0; Y < gridSize; Y++)
+                {
+                    var t = new Vector3(
+                        (X - gridSize / 2) * scale,
+                        (Y - gridSize / 2) * scale,
+                        0);
+                    createdTransforms.Add(t);
+                }
+            }
+            return createdTransforms.ToArray();
+        }
+        public static Vector3[] SquareGrid2DHorizontal(int gridSize, float scale)
+        {
+            var createdTransforms = new List<Vector3>();
+            for (int X = 0; X < gridSize; X++)
+            {
+                for (int Y = 0; Y < gridSize; Y++)
+                {
+                    var t = new Vector3(
+                        (X - gridSize / 2) * scale,
+                        0,
+                       (Y - gridSize / 2) * scale);
+                    createdTransforms.Add(t);
+                }
+            }
+            return createdTransforms.ToArray();
+        }
+        public static Vector3[] SquareGrid3D(int gridSize, float scale)
+        {
+            var createdTransforms = new List<Vector3>();
+            for (int X = 0; X < gridSize; X++)
+            {
+                for (int Y = 0; Y < gridSize; Y++)
+                {
+                    for (int Z = 0; Z < gridSize; Z++)
+                    {
+                        var t = new Vector3(
+                             (X - gridSize / 2) * scale,
+                             (Y - gridSize / 2) * scale,
+                             (Z - gridSize / 2) * scale);
+                        createdTransforms.Add(t);
+                    }
+                }
+            }
+            return createdTransforms.ToArray();
+        }
+    }
+}
