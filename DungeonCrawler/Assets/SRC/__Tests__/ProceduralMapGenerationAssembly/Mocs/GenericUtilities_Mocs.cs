d4b976ca381b44429f62d238518c4efe26c2b4b0
Peter Francois Badenhorst <peter.francois.badenhorst@gmail.com>
2023-01-03 20:03:29 +0200

    Validate_NeighborsPosition Fix

---
+++ DungeonCrawler/Assets/SRC/__Tests__/ProceduralMapGenerationAssembly/Mocs/GenericUtilities_Mocs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.SRC.Tests.Assets.SRC.__Tests__.ProceduralMapGenerationAssembly.Mocs
+{
+    internal class GenericUtilities_Mocs
+    {
+        public Vector3[] SetUpNeighbors(float scale)
+        {
+
+            return new Vector3[]
+                {
+                    Vector3.right* scale,
+                    Vector3.forward * scale,
+                    Vector3.left* scale,
+                    Vector3.back* scale,
+                    Vector3.up* scale,
+                    Vector3.down* scale
+                 };
+        }
+    }
+}
--- DungeonCrawler/Assets/SRC/__Tests__/ProceduralMapGenerationAssembly/Utilities/GenericUtilities_Tests.cs
+++ DungeonCrawler/Assets/SRC/__Tests__/ProceduralMapGenerationAssembly/Utilities/GenericUtilities_Tests.cs
@@ -2,35 +2,52 @@
 using NUnit.Framework;
 using FluentAssertions;
 using UnityEngine;
+using Assets.SRC.Tests.Assets.SRC.__Tests__.ProceduralMapGenerationAssembly.Mocs;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Assets.SRC.ProceduralMapGeneration.Utilities;
+using System.Linq;
+using FluentAssertions;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UIElements;
+using Assets.SRC.ProceduralMapGeneration.Structs;
+using Assets.SRC.ProceduralMapGeneration.Enums;
+using Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
+using Assets.SRC.ProceduralMapGeneration.Utilities.Tests;
 
 namespace Assets.SRC.ProceduralMapGeneration.Utilities.Test
 {
     public class GenericUtilities_Tests
     {
-        private Vector3[] SetUpNeighbors(float scale)
+        private GenericUtilities_Mocs _genericUtilities_Mocs;
+        #region [SetUp]&&[TearDown]
+        [SetUp]
+        public void SetUp()
         {
-
-            return new Vector3[]
-                {
-                    Vector3.right* scale,
-                    Vector3.forward * scale,
-                    Vector3.left* scale,
-                    Vector3.back* scale,
-                    Vector3.up* scale,
-                    Vector3.down* scale
-                 };
+            _genericUtilities_Mocs= new GenericUtilities_Mocs();
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            _genericUtilities_Mocs= null;
+        }
+        #endregion
 
+        #region Validate_NeighborsPosition Test Cases
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(5)]
         [TestCase(25)]
         [TestCase(44.2f)]
-        public void NeighborsPosition(float scale)
+        #endregion
+        public void Validate_NeighborsPosition(float scale)
         {
             var result = GenericUtilities.NeighborsPosition(scale);
-            result.Should().BeEquivalentTo(SetUpNeighbors(scale));
+            result.Should().BeEquivalentTo(_genericUtilities_Mocs.SetUpNeighbors(scale));
         }
     }
 }
