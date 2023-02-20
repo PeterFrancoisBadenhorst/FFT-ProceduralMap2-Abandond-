a0e5cc27e0b90749916b7909fab3d5c2444a563c
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-02-20 18:15:01 +0200

    Fixed Test Runner

---
+++ DungeonCrawler/Assets/SRC/__Tests__/ProceduralMapGenerationAssembly/Mono/UtilitiesBehaviour_Tests.cs
@@ -0,0 +1,12 @@
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Numerics;
+using UnityEngine;
+
+namespace Assets.SRC.ProceduralMapGeneration.Mono.Test
+{
+    public class UtilitiesBehaviour_Tests: MonoBehaviour
+    {
+    }
+}
