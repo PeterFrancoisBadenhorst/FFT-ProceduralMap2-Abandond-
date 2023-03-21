b0a86969e4dc7eeb5690d65b47e16e759881b445
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-03-21 08:31:10 +0200

    Path Finding Rework

---
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/PathFinding/NewNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.PathFinding
+{
+    internal class NewNode
+    {
+        public Vector3 Position { get; set; }
+        public List<NewNode> Neighbors { get; set; }
+        public bool Closed { get; set; }
+        public float fCost { get; set; }
+        public float hCost { get; set; }
+    }
+}
---
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/PathFinding/NewPathFinding.cs
@@ -0,0 +1,118 @@
+﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.PathFinding;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.Shared.Assets.SRC.Shared.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.PathFinding
+{
+    internal class NewPathFinding
+    {
+        /// <summary>
+        /// H Cost => Diff to neighbors
+        /// F Cost => Diff to end point
+        /// ToDo:
+        ///  Get grid
+        ///  create node per pos in grid   -
+        ///  end-start node chooser
+        ///  Set Neighbors of Nodes -
+        ///  Itterate through Neighbors to get to end node
+        /// </summary>
+        VectorMath vMath = new VectorMath();
+        private List<NewNode> NodeGridCreator(Vector3[] grid, Vector3 endPos)
+        {
+            List<NewNode> nodes = new();
+            for (int i = 0; i < grid.Length; i++)
+            {
+
+                NewNode node = new();
+                node.Position = grid[i];
+                node.fCost = vMath.CalculateDistanceBetweenTwoVectors(node.Position, endPos);
+                nodes.Add(node);
+            }
+            return nodes;
+        }
+        private List<NewNode> SetNodeNeighbors(List<NewNode> nodes, float scale)
+        {
+            /// <summary>
+            ///     N 1;0;0
+            ///     E 0;0;1
+            ///     S -1;0;0
+            ///     W 0;0;-1
+            ///     T 0;1;0
+            ///     B 0;-1;0
+            /// </summary>
+            List<NewNode> setNodes = new();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                for (int g = 0; g < nodes.Count; g++)
+                {
+                    List<NewNode> neighbornodes = new();
+                    var currentNode = nodes[i];
+                    /// North
+                    if (currentNode.Position.x == nodes[g].Position.x + scale &&
+                        currentNode.Position.y == nodes[g].Position.y &&
+                        currentNode.Position.z == nodes[g].Position.z
+                        )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    /// East
+                    if (currentNode.Position.x == nodes[g].Position.x &&
+                       currentNode.Position.y == nodes[g].Position.y &&
+                       currentNode.Position.z == nodes[g].Position.z + scale
+                       )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    /// South 
+                    if (currentNode.Position.x == nodes[g].Position.x - scale &&
+                         currentNode.Position.y == nodes[g].Position.y &&
+                         currentNode.Position.z == nodes[g].Position.z
+                        )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    /// West
+                    if (currentNode.Position.x == nodes[g].Position.x &&
+                       currentNode.Position.y == nodes[g].Position.y &&
+                       currentNode.Position.z == nodes[g].Position.z - scale
+                       )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    /// Top
+                    if (currentNode.Position.x == nodes[g].Position.x &&
+                           currentNode.Position.y == nodes[g].Position.y + scale &&
+                           currentNode.Position.z == nodes[g].Position.z
+                           )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    /// Bottom
+                    if (currentNode.Position.x == nodes[g].Position.x &&
+                           currentNode.Position.y == nodes[g].Position.y - scale &&
+                           currentNode.Position.z == nodes[g].Position.z
+                           )
+                    {
+                        nodes[g].hCost = currentNode.fCost - nodes[g].fCost;
+                        neighbornodes.Add(nodes[g]);
+                        //break;
+                    }
+                    currentNode.Neighbors = neighbornodes;
+                    setNodes.Add(currentNode);
+                }
+            }
+            return setNodes;
+        }
+    }
+}
