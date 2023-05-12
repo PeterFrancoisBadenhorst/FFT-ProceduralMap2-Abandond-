a4ae0cbdecefb765e188d2804486851ccab52fc0
PeterFrancoisBadenhorst <45225120+PeterFrancoisBadenhorst@users.noreply.github.com>
2023-05-12 20:20:42 +0200

    Update ChunkBehavior.cs

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Behaviors/ChunkBehavior.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Behaviors/ChunkBehavior.cs
@@ -1,13 +1,6 @@
-﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Utilities;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Structs;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.ScriptableObjects;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Noise;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Managers;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Enums;
 using UnityEngine;
 
-namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors
+namespace PathFinding
 {
     public class ChunkBehavior : MonoBehaviour
     {
