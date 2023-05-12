cffca06f19f40da23485ca8aab8777464f469299
PeterFrancoisBadenhorst <45225120+PeterFrancoisBadenhorst@users.noreply.github.com>
2023-05-12 20:22:34 +0200

    Update UtilitiesBehaviour.cs

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Behaviors/UtilitiesBehaviour.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Behaviors/UtilitiesBehaviour.cs
@@ -1,26 +1,12 @@
-﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Utilities;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Structs;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.ScriptableObjects;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Noise;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Managers;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
-using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Enums;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors
+namespace PathFinding
 {
-    internal class UtilitiesBehaviour : MonoBehaviour
+    public class UtilitiesBehaviour : MonoBehaviour
     {
-        public static GameObject InstantiateObject(GameObject gObject, Vector3 position, Quaternion rotation)
+        public static GameObject InstantiateObject(GameObject gObject, Vector3 position = Vector3.zero, Quaternion rotation = Quaternion.identity)
         {
-            return Instantiate(gObject, Vector3.zero, rotation);
+            return Instantiate(gObject, position, rotation);
         }
         public static void PurgeObject(GameObject obj)
         {
