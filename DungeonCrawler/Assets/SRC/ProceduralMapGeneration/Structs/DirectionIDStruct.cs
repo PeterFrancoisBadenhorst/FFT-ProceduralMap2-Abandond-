de9bda0e922e82c5719d26ad1f10d444eedba41d
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-02 12:21:01 +0200

    DirectionIDStruct

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/DirectionIDStruct.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/DirectionIDStruct.cs
@@ -1,12 +1,19 @@
 ﻿namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Structs
 {
+    // This struct represents a set of directions.
     public struct DirectionIDStruct
     {
+        // Whether the north direction is valid.
         public bool NothID { get; set; }
+        // Whether the east direction is valid.
         public bool EastID { get; set; }
+        // Whether the west direction is valid.
         public bool WestID { get; set; }
+        // Whether the south direction is valid.
         public bool SouthID { get; set; }
+        // Whether the top direction is valid.
         public bool TopID { get; set; }
+        // Whether the bottom direction is valid.
         public bool BottomID { get; set; }
     }
 }
