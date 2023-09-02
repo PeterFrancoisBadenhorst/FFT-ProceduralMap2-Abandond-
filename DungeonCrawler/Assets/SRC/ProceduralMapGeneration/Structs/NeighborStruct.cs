ce092f3ad394dd8891f07c5bc7f380c045b00e11
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-02 12:21:22 +0200

    NeighborStruct

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/NeighborStruct.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/NeighborStruct.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 
 namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Structs
 {
+    // This struct represents a neighbor of a chunk.
     [System.Serializable]
     public struct NeighborStruct
     {
+        // The direction of the neighbor.
         public DirectionTypeEnum Direction;
+        // The origin object of the neighbor.
         public GameObject OriginObject;
+        // The north neighbor of the chunk.
         public GameObject NorthNeighbor;
+        // The east neighbor of the chunk.
         public GameObject EastNeighbor;
+        // The south neighbor of the chunk.
         public GameObject SouthNeighbor;
+        // The west neighbor of the chunk.
         public GameObject WestNeighbor;
+        // The top neighbor of the chunk.
         public GameObject TopNeighbor;
+        // The bottom neighbor of the chunk.
         public GameObject BottomNeighbor;
     }
 }
