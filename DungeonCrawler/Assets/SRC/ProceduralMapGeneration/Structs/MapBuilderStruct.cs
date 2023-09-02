f908ec78dcc7211423a357be7e7cbf7266c81b51
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-02 12:21:14 +0200

    MapBuilderStruct

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/MapBuilderStruct.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Structs/MapBuilderStruct.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Structs
 {
+    // This class represents the data needed to build a map.
     public class MapBuilderStruct
     {
+        // The grid of GameObjects that make up the map.
         public List<GameObject> grid { get; set; }
+        // The starting object on the map.
         public GameObject startObject { get; set; }
+        // The previous tile position. This is used to track the player's movement.
         public GameObject previousTilePos { get; set; }
     }
 }
