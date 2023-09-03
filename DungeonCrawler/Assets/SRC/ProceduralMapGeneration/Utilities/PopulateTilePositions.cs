f57569b5f28c37b666c0c86f148d276f6c233fd5
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-03 19:26:15 +0200

    Refactor

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Utilities/PopulateTilePositions.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Utilities/PopulateTilePositions.cs
@@ -1,4 +1,6 @@
-﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
+﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Global;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Modles;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Behaviors;
 using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.ScriptableObjects;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,32 +17,32 @@ namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.
          *
          * @return The list of parent objects.
          */
-        public List<GameObject> SetChildTile(DirectionalTilesScriptableObject tolCol, List<GameObject> parentObjects)
+        public List<GameObject> SetChildTile(ProcedualMapGenCreationModel CreationModel)
         {
             // Get the list of child tiles from the directional tiles scriptable object.
-            var j = tolCol.RetunObjectsAsAList();
+            var RoomCount = GlobalVariables.CreationModel.Rooms.RetunObjectsAsAList();
 
             // Iterate through the parent objects.
-            for (int i = 0; i < parentObjects.Count; i++)
+            for (int i = 0; i < RoomCount.Count; i++)
             {
                 // Get the child tile index for the current parent object.
-                int g = (int)parentObjects[i].GetComponent<ChunkBehavior>().neighborStruct.Direction;
+                int g = (int)GlobalVariables.CreationModel.GridRelations[i].GetComponent<ChunkBehavior>().neighborStruct.Direction;
 
                 // Instantiate the child tile at the position of the parent object.
-                GameObject t = UtilitiesBehaviour.InstantiateObject(j[g], parentObjects[i].transform.position, Quaternion.identity);
+                GameObject t = UtilitiesBehaviour.InstantiateObject(RoomCount[g], GlobalVariables.CreationModel.GridRelations[i].transform.position, Quaternion.identity);
 
                 // Set the parent-child relationship between the parent object and the child tile.
-                t.transform.SetParent(parentObjects[i].transform, false);
+                t.transform.SetParent(GlobalVariables.CreationModel.GridRelations[i].transform, false);
 
                 // Make the parent object static.
-                parentObjects[i].isStatic = true;
+                GlobalVariables.CreationModel.GridRelations[i].isStatic = true;
 
                 // Set the name of the parent object to its index and position.
-                parentObjects[i].name = i + "   " + parentObjects[i].transform.position.ToString();
+                GlobalVariables.CreationModel.GridRelations[i].name = i + "   " + GlobalVariables.CreationModel.GridRelations[i].transform.position.ToString();
             }
 
             // Return the list of parent objects.
-            return parentObjects;
+            return GlobalVariables.CreationModel.GridRelations;
         }
     }
 }
