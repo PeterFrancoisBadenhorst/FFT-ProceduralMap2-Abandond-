b68bcd7c054f804c9b93bf448f8680faaa9a1478
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-03 19:26:01 +0200

    Refactor

---
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Modles/ProcedualMapGenCreationModel.cs
@@ -0,0 +1,34 @@
+﻿using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.PathFinding;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Enums;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.ScriptableObjects;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Modles
+{
+    public class ProcedualMapGenCreationModel
+    {
+        public GameObject GridParent;
+        public GameObject PlayerPrefab;
+        public int GridSize;
+        public int MapTotalFillPercentage;
+        public float GridScale;
+        public GridTypeEnum GridType;
+        public DirectionalTilesScriptableObject Rooms;
+
+        public Vector3[] Grid;
+        public Vector3[] MapGrid;
+        public List<Vector3> MapTotal;
+        public List<GameObject> GridRelations = new();
+
+        public readonly PopulateTilePositions _populateTilePositionsBehavior = new();
+        public readonly GridCreate _gridCreate = new();
+        public readonly ChunkHandler _chunkHandler = new();
+        public readonly NewPathFinding _newPathFinding = new();
+    }
+}
