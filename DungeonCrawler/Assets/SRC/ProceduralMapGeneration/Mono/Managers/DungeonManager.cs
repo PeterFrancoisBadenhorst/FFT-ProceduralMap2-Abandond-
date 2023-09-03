095b20fccdc3c8cdf98d1e1d4472dbec1f875c67
Peter Francois Badenhorst <Peter.Francois.Badenhorst@gmail.com>
2023-09-03 19:25:50 +0200

    Refactor

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Managers/DungeonManager.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Mono/Managers/DungeonManager.cs
@@ -3,11 +3,14 @@ using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Enum
 using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.ScriptableObjects;
 using Assets.SRC.ProceduralMapGeneration.Assets.SRC.Shared.mono;
 using UnityEngine;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Modles;
+using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Global;
 
 namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Mono.Managers
 {
     internal class DungeonManager : MonoBehaviour
     {
+        
         public GameObject GridParent;
         public int GridSize;
         public float GridScale;
@@ -20,7 +23,21 @@ namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.
         public DirectionalTilesScriptableObject scriptRef;
         private readonly PathMapBuilder _pathMapBuilder = new();
 
-        private void OnEnable() => _pathMapBuilder.CreateMap(GridSize, GridScale, GridParent.transform, scriptRef, MapTotalFillPercentage, GridType);
+
+        private void Awake() => _pathMapBuilder.CreateMap(PopulateCreationModel());
+        
+        private ProcedualMapGenCreationModel PopulateCreationModel()
+        {
+            if (GlobalVariables.CreationModel == null) GlobalVariables.CreationModel = new();
+            GlobalVariables.CreationModel.GridParent = GridParent;
+            GlobalVariables.CreationModel.GridSize = GridSize;
+            GlobalVariables.CreationModel.GridScale = GridScale;
+            GlobalVariables.CreationModel.GridType = GridType;
+            GlobalVariables.CreationModel.PlayerPrefab = PlayerPrefab;
+            GlobalVariables.CreationModel.MapTotalFillPercentage = MapTotalFillPercentage;
+            GlobalVariables.CreationModel.Rooms = scriptRef;
+            return GlobalVariables.CreationModel;
+        }
 
     }
 }
