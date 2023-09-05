a1e887d6342c9da2873851877c2b403e4201d069
Peter Francois Badenhorst <peter.francois.badenhorst@gmail.com>
2023-09-05 17:05:08 +0200

    Refactor

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Global/GlobalVariables.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Global/GlobalVariables.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Modles;
 
 namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.Global
