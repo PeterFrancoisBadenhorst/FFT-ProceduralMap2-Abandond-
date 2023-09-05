67c9cda3d0d4c54e9b5b8d8d5b18b81f9b6cccb7
Peter Francois Badenhorst <peter.francois.badenhorst@gmail.com>
2023-09-05 17:05:12 +0200

    Refactor

--- DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Models/NewNodeModel.cs
+++ DungeonCrawler/Assets/SRC/ProceduralMapGeneration/Models/NewNodeModel.cs
@@ -6,10 +6,10 @@ namespace Assets.SRC.ProceduralMapGeneration.Assets.SRC.ProceduralMapGeneration.
     // Define a class representing a node in a pathfinding graph
     public class NewNodeModel
     {
-        public Vector3 Position;                // Position of the node
+        public Vector3 Position;                     // Position of the node
         public List<NewNodeModel> Neighbors;         // List of neighboring nodes
-        public bool Closed;                     // Whether the node has been visited
-        public float fCost, hCost;              // Cost variables used in A* algorithm
+        public bool Closed;                          // Whether the node has been visited
+        public float fCost, hCost;                   // Cost variables used in A* algorithm
         public NewNodeModel LastNode;                // The last node visited in the path
     }
 }
